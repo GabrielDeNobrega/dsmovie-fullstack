77b2ceebbdd0092d0e866c28e9fe1e0337868b5f
GabrielDeNobrega <sr.gabrielnobrega@hotmail.com>
2022-05-20 17:45:12 -0300

    Navbar Links

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,12 +8,24 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/devsuperior">
-                        <div className="dsmovie-contact-container">
-                            <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                    <div className="dsmovie-nav-content-links">
+                        <div className="dsmovie-nav-content-links">
+                            <a href="https://github.com/GabrielDeNobrega">
+                                <div className="dsmovie-contact-container">
+                                    <GithubIcon />
+                                    <p className="dsmovie-contact-link">/GabrielDeNobrega</p>
+                                </div>
+                            </a>
                         </div>
-                    </a>
+                        <div className="dsmovie-nav-content-links">
+                            <a href="https://github.com/devsuperior">
+                                <div className="dsmovie-contact-container">
+                                    <GithubIcon />
+                                    <p className="dsmovie-contact-link">/devsuperior</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </header>
